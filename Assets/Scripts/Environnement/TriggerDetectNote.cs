e37ec9d30aefddc06085c43d372428afc30cabc2
Vincent <carville.pro@gmail.com>
2020-12-02 06:13:05 +0100

    Vas y je merge je vais me coucher ca ma saoulé

---
+++ Assets/Scripts/Environnement/TriggerDetectNote.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetectNote : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private GameObject _firstPlatform;
+    [SerializeField] private bool _firstNotePassed;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            if (!_firstNotePassed)
+            {
+                _spriteRenderer.enabled = false;
+                _firstPlatform.SetActive(false);
+                _firstNotePassed = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -6,15 +6,17 @@ public class MovePlayer : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody2D;
     [SerializeField] private Vector2 _velocity;
-    [SerializeField] private float _lengt, _coefTimeFall, _timeFall, _forceJump;
+    [SerializeField] private float _speed, _lengt, _coefTimeFall, _timeFall, _forceJump, _timeInJump, _timeInJumpMax;
     [SerializeField] private LayerMask _layerMask;
-    [SerializeField] private bool _isGrounded, _pressJump, _enterGround;
+    [SerializeField] private bool _isGrounded, _enterGround, _pressJump, _isInJump;
+
+    [SerializeField] private Transform _worldParrent;
 
     private void Update()
     {
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            _velocity.x = Input.GetAxisRaw("Horizontal");
+            _velocity.x = Input.GetAxisRaw("Horizontal") * _speed;
             //transform.position = new Vector2(transform.position.x + Input.GetAxisRaw("Horizontal") / 10f, 0);
         }
         else
@@ -31,7 +33,7 @@ public class MovePlayer : MonoBehaviour
             _pressJump = false;
         }
 
-        if(_isGrounded)
+        if(_isGrounded || _isInJump)
         {
             _timeFall = 0;
         }
@@ -45,7 +47,7 @@ public class MovePlayer : MonoBehaviour
 
     private void FixedUpdate()
     {
-        RayCastTouchFloor();
+        //RayCastTouchFloor();
         Fall();
         Jump();
         _rigidbody2D.velocity = _velocity;
@@ -60,11 +62,59 @@ public class MovePlayer : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("ZIZI");
+        if(collision.gameObject.layer == 9)
+        {
+            Debug.Log("ZI2ZI2");
+
+            _isGrounded = true;
+            if (!_enterGround)
+            {
+                _velocity.y = 0;
+                _enterGround = true;
+            }
+
+            transform.SetParent(collision.gameObject.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            _isGrounded = false;
+            _enterGround = false;
+            transform.SetParent(null);
+        }
+    }
+
     private void Jump()
     {
         if (_isGrounded && _pressJump)
         {
-            _rigidbody2D.AddForce( new Vector2(0, _forceJump), ForceMode2D.Force);
+            _isInJump = true;
+            //_rigidbody2D.AddForce( new Vector2(0, _forceJump), ForceMode2D.Force);
+        }
+
+        if(_isInJump && _pressJump)
+        {
+            if(_timeInJump < _timeInJumpMax)
+            {
+                _timeInJump += Time.deltaTime;
+                _rigidbody2D.AddForce(new Vector2(0, _forceJump - _timeInJump / 100), ForceMode2D.Force);
+            }
+            else
+            {
+                _timeInJump = 0;
+                _isInJump = false;
+            }
+        }
+        else
+        {
+            _timeInJump = 0;
+            _isInJump = false;
         }
     }
 
@@ -79,7 +129,7 @@ public class MovePlayer : MonoBehaviour
             // Calculate the distance from the surface and the "error" relative
             // to the floating height.
             float distance = Mathf.Abs(hit.point.y - transform.position.y);
-            Debug.Log("Distance = " + distance);
+            //Debug.Log("Distance = " + distance);
             Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
 
             if (distance < 0.02f)
