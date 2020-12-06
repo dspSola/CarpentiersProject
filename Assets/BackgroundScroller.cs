429fc6367baea725662e09878e61ccbb4ae0f28b
Jello <lu.emmanuel@laposte.net>
2020-12-07 00:29:53 +0100

    Auto stash before merge of "Lukas" and "main"

---
+++ Assets/BackgroundScroller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float _scrollSpeed;
+
+    void Update()
+    {
+        transform.position -= new Vector3(transform.position.x, _scrollSpeed, transform.position.z);
+    }
+}
