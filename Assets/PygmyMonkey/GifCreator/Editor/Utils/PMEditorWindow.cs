020ac3627a192f3636b737f90bddf55828681d6e
DspSola <robin.sola@gmail.com>
2020-12-07 00:58:49 +0100

    gif creator

---
+++ Assets/PygmyMonkey/GifCreator/Editor/Utils/PMEditorWindow.cs
@@ -0,0 +1,100 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PygmyMonkey.GifCreator.Utils
+{
+	public abstract class PMEditorWindow : EditorWindow
+	{
+		protected static EditorWindow createWindow<T>(string productName) where T : EditorWindow
+		{
+			T window = EditorWindow.GetWindow<T>(false);
+			window.titleContent = new GUIContent(" " + getShortProductName(productName), (Texture2D)AssetDatabase.LoadAssetAtPath(getIconPath(productName), typeof(Texture2D)));
+			return window;
+		}
+
+		public abstract string getProductName();
+		public abstract string getVersionName();
+		public abstract string getAssetStoreID();
+
+		private static string getIconPath(string productName)
+		{
+			return "Assets/PygmyMonkey/" + getFormattedProductName(productName) + "/Editor/Textures/Icon.png";
+		}
+
+		protected static string getFormattedProductName(string productName)
+		{
+			return productName.Replace(" ", string.Empty).Trim();
+		}
+
+		private static string getShortProductName(string productName)
+		{
+			if (productName.Length <= 13)
+			{
+				return productName;
+			}
+
+			return productName.Substring(0, 13) + ".";
+		}
+
+		public abstract void init();
+
+		private void OnEnable()
+		{
+			hideFlags = HideFlags.HideAndDontSave;
+
+			init();
+		}
+		
+		private void OnFocus()
+		{
+			Repaint();
+		}
+		
+		private void OnSelectionChange()
+		{
+			Repaint();
+		}
+
+		private Vector2 scrollPosition;
+		public virtual void drawBegin() {}
+		public abstract void drawContent();
+		public virtual void drawEnd() {}
+
+		protected bool useCustomGUI = false;
+		public virtual void drawCustomGUI() {}
+
+		private void OnGUI()
+		{
+			if (useCustomGUI)
+			{
+				drawCustomGUI();
+				return;
+			}
+
+			drawBegin();
+
+			using (new GUIUtils.GUIScrollView(ref scrollPosition))
+			{
+				using (new GUIUtils.GUIHorizontal())
+				{
+					GUILayout.Space(5f);
+					using (new GUIUtils.GUIVertical())
+					{
+						GUILayout.Space(5f);
+						
+						EditorGUILayout.LabelField(getProductName() + " - Version " + getVersionName(), LegacyGUIStyle.LabelVersionStyle);
+
+						drawContent();
+
+						GUILayout.Space(5f);
+						PMUtils.DrawAskToRate(getFormattedProductName(getProductName()).ToLower() + "_show_rate_popup", getProductName(), getAssetStoreID());
+						GUILayout.Space(10f);
+					}
+					GUILayout.Space(5f);
+				}
+			}
+
+			drawEnd();
+		}
+	}
+}
---
+++ Assets/PygmyMonkey/GifCreator/Scripts/Runtime/CustomRectPreview.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Reflection;
+
+namespace PygmyMonkey.GifCreator
+{
+	[ExecuteInEditMode]
+	public class CustomRectPreview : MonoBehaviour
+	{
+		public static CustomRectPreview Singleton;
+
+		private Material lineMaterial;
+		private RecorderParameters parameters;
+
+		private float startX;
+		private float startY;
+		private float endX;
+		private float endY;
+
+		private bool show = false;
+		
+		public void OnEnable()
+		{
+			Singleton = this;
+		}
+
+		private void createLineMaterial()
+		{
+			// Unity has a built-in shader that is useful for drawing simple colored things.
+			lineMaterial = new Material(Shader.Find("Hidden/Internal-Colored"));
+			lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+		}
+
+		public void Show(RecorderParameters parameters)
+		{
+			this.parameters = parameters;
+
+			show = true;
+			repaintGameView();
+		}
+
+		public void Hide()
+		{
+			show = false;
+			repaintGameView();
+		}
+
+		// Will be called after all regular rendering is done
+		void OnRenderObject()
+		{
+			if (!show)
+			{
+				return;
+			}
+
+			if (lineMaterial == null)
+			{
+				createLineMaterial();
+			}
+
+			if (parameters.section == RecorderParameters.Section.CustomRectAbsolute)
+			{
+				startX = parameters.absolutePosX / parameters.getMainGameViewSize().x;
+				startY = parameters.absolutePosY / parameters.getMainGameViewSize().y;
+				endX = (parameters.absolutePosX + parameters.absoluteWidth) / parameters.getMainGameViewSize().x;
+				endY = (parameters.absolutePosY + parameters.absoluteHeight) / parameters.getMainGameViewSize().y;
+			}
+			else if (parameters.section == RecorderParameters.Section.CustomRectRelative)
+			{
+				startX = parameters.relativePosX;
+				startY = parameters.relativePosY;
+				endX = parameters.relativePosX + parameters.relativeWidth;
+				endY = parameters.relativePosY + parameters.relativeHeight;
+			} 
+
+			GL.PushMatrix();
+			{
+				lineMaterial.SetPass(0);
+
+				GL.LoadOrtho();
+				GL.Begin(GL.QUADS);
+				{
+					GL.Color(parameters.customRectPreviewColor);
+					GL.Vertex3(startX, startY, 0);
+					GL.Vertex3(startX, endY, 0);
+					GL.Vertex3(endX, endY, 0);
+					GL.Vertex3(endX, startY, 0);
+				}
+				GL.End();
+			}
+			GL.PopMatrix();
+		}
+
+		private void repaintGameView()
+		{
+			#if UNITY_EDITOR
+			Type gameViewTypee = GetGameViewType();
+			EditorWindow gameViewWindow = GetGameViewWindow(gameViewTypee);
+            if (gameViewWindow != null)
+            {
+				gameViewWindow.Repaint();
+			}
+            #endif
+		}
+
+        #if UNITY_EDITOR
+		private static Type GetGameViewType()
+		{
+			Assembly unityEditorAssembly = typeof(EditorWindow).Assembly;
+			return unityEditorAssembly.GetType("UnityEditor.GameView");
+		}
+        #endif
+
+		private static EditorWindow GetGameViewWindow(Type gameViewType)
+		{
+			UnityEngine.Object[] obj = Resources.FindObjectsOfTypeAll(gameViewType);
+			if (obj.Length > 0)
+			{
+				return obj[0] as EditorWindow;
+			}
+
+			return null;
+		}
+	}
+}
